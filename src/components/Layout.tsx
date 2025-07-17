03a20fa7fdb2f6a5e27b8defbee1e6b4249bae69
jvharshal <j.v.harshal@gmail.com>
2025-07-17 19:56:24 +0530

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,22 @@
-import { Outlet } from 'react-router-dom';
+// src/components/Layout.tsx
+import React, { ReactNode } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import FloatingCTA from './FloatingCTA';
 
-export default function Layout() {
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <>
+    <div className="flex flex-col min-h-screen bg-white">
       <Header />
-      <main>
-        <Outlet />
-      </main>
+      <main className="flex-grow pt-20">{children}</main>
       <Footer />
       <FloatingCTA />
-    </>
+    </div>
   );
-}
+};
+
+export default Layout;
