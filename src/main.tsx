75bbb07628f1d8aaaf542d373e5fe1e8f4b27b3b
jvharshal <j.v.harshal@gmail.com>
2025-07-19 09:41:45 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
+// src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/Mozno_Wesite_2.0">
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+root.render(
+  <BrowserRouter basename="/Mozno_Wesite_2.0">
+    <App />
+  </BrowserRouter>
 );
